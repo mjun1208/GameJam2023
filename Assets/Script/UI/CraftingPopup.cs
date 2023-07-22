68cd8a8a659464bcf2cdbda28a8d2535934f7327
mjun1208 <mjun1208@naver.com>
2023-07-23 06:35:05 +0900

    팝업 위치 잘못 잡아주는 버그 수정

--- Assets/Script/UI/CraftingPopup.cs
+++ Assets/Script/UI/CraftingPopup.cs
@@ -1,13 +1,23 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class CraftingPopup : MonoBehaviour
 {
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private RectTransform _canvas;
+    
     [SerializeField] private TMP_Text _levelText;
     [SerializeField] private TMP_Text _levelUpCost;
     [SerializeField] private GameObject DisableButton;
-    
+
+    private RectTransform _rectTransform;
     private CraftingTable _craftingTable;
+    
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
 
     public void SetInfo(CraftingTable craftingTable)
     {
@@ -19,6 +29,21 @@ public class CraftingPopup : MonoBehaviour
         UpdateStars();
 
         Enable();
+    }
+
+    private void Update()
+    {
+        if (_craftingTable != null)
+        {
+            Vector2 ViewportPosition = _mainCamera.WorldToViewportPoint(_craftingTable.transform.position);
+            Vector2 WorldObject_ScreenPosition = new Vector2(
+                ((ViewportPosition.x * _canvas.sizeDelta.x) - (_canvas.sizeDelta.x * 0.5f)),
+                ((ViewportPosition.y * _canvas.sizeDelta.y) - (_canvas.sizeDelta.y * 0.5f)) + 20);
+            _rectTransform.anchoredPosition = WorldObject_ScreenPosition;
+            
+            // this.transform.position =
+            //     new Vector3(_craftingTable.transform.position.x, _craftingTable.transform.position.y - 20, this.transform.position.z);
+        }
     }
 
     private void UpdateStars()
