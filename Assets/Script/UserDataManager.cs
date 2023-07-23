126938c89c94af30d8ca5e4278a7a6f0c115949a
JeongHyeokBang <hyeok020514@gmail.com>
2023-07-23 09:12:54 +0900

    퀘스트 슬롯 어쩌구 모시깽 Let's go

--- Assets/Script/UserDataManager.cs
+++ Assets/Script/UserDataManager.cs
@@ -6,4 +6,34 @@ public class UserDataManager
     public int Jewelry { get; set; }
     
     public int Stage { get; set; }
+
+
+    //여긴.. 지옥. 이다.
+    public int Value_AddAnimal { get; private set; }
+    public float Value_HammerSpawnDelay { get; private set; } = 1;
+    public float Value_AnimalGold { get; private set; } = 1;
+    public int Value_SpawnPlayer { get; private set; } = 0;
+    public float Value_PlayerSpeed { get; private set; } = 1;
+
+    public void Purchase(Quest quest)
+    {
+        switch (quest.QuestType)
+        {
+            case QuestType.AddAnimal:
+                Value_AddAnimal += (int)quest.GrowValue;
+                break;
+            case QuestType.HammerSpawnDelay:
+                Value_HammerSpawnDelay *= (int)quest.GrowValue;
+                break;
+            case QuestType.AnimalGold:
+                Value_AnimalGold += (int)quest.GrowValue;
+                break;
+            case QuestType.SpawnPlayer:
+                Value_SpawnPlayer += (int)quest.GrowValue;
+                break;
+            case QuestType.PlayerSpeed:
+                Value_PlayerSpeed += (int)quest.GrowValue;
+                break;
+        }
+    }
 }
