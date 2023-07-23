aede4b2f464ce63e0f72eadfa9a72998e1a7242a
LetsGo <mjun1208@naver.com>
2023-07-23 12:31:54 +0900

    끼끼룽

--- Assets/Script/UserDataManager.cs
+++ Assets/Script/UserDataManager.cs
@@ -2,7 +2,7 @@ public class UserDataManager
 {
     // 이것들만 저장하면 행복해질까..?
 
-    public int Gold { get; set; };
+    public int Gold { get; set; } = 0;
     public int Jewelry { get; set; }
     
     public int Stage { get; set; }
